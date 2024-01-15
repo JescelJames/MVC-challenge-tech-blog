85d73b617026196952bde48a91507be27670f253
Jescel James <jesceljames@gmail.com>
2024-01-14 22:42:08 -0500

    added handlebars requirements in server.js

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 //REQUIREMENTS________________
   const express = require('express');
+  const path = require('path');
+  const exphbs = require('express-handlebars');
   const session = require('express-session');
+  const SequelizeStore = require('connect-session-sequelize')(session.Store);
   const routes = require('./controllers');
   const sequelize = require('./config/connection');
-  const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 // APP/PORT______________________
   const app = express();
   const PORT = process.env.PORT || 3001;
+
+// HANDLEBARS SETUP  
+  const hbs = exphbs.create({});
+  // Set Handlebars as the default template engine.
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+
 // //SESSION _______________________
+
 //   const sess = {
 //     secret: 'Super secret secret',
 //     cookie: {},
@@ -18,9 +29,11 @@
 //     })
 //   };
 //   app.use(session(sess));
+
 //MIDDLEWARES_________________  
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
 //ROUTES___________________
   app.use(routes);
 //START SERVER________________________
