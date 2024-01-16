4bb49ee99a6f4dcdeb34988db169c907c630c0d4
Jescel James <jesceljames@gmail.com>
2024-01-15 23:40:36 -0500

    handlebar views renders

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 
 //test test test
-router.get('/', (req, res) => res.send('Navigate to /send or /routes. jcv'));
+// router.get('/', (req, res) => res.send('Navigate to /send or /routes. jcv'));
 
+
+router.get('/', async (req, res) => {
+  res.render('all');
+});
 module.exports = router;
