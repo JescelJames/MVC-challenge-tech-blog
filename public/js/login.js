45d063c478d10fc13d68a7750fbd09b6385e502a
Jescel James <jesceljames@gmail.com>
2024-01-21 21:22:11 -0500

    login.js frontendjs ready for check

--- public/js/login.js
+++ public/js/login.js
@@ -4,5 +4,23 @@ async function loginFormHandler(event) {
   const username = document.querySelector('#username').value.trim();
 
 
-  
+  if (username) {
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        username
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+
+    } else {
+      alert(response.statusText);
+    }
+  }
 }
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
