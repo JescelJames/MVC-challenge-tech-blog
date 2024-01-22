d82f70e1674d7150aa138bacddcddb0b54330d79
Jescel James <jesceljames@gmail.com>
2024-01-21 20:10:22 -0500

    added homroute in controller index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
+const homeRoutes = require('./homeroute.js');
 
 
 //test test test
 // router.get('/', (req, res) => res.send('Navigate to /send or /routes. jcv'));
 
 
-router.get('/', async (req, res) => {
-  res.render('all');
-});
+router.use('/', homeRoutes);
+
+
+
 module.exports = router;
