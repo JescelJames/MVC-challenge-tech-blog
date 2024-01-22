ead128842d88c1f1eb4d9a6a9e7aa8380bbb35af
Jescel James <jesceljames@gmail.com>
2024-01-21 21:44:37 -0500

    checkout to a different branch for signup

--- controllers/homeroute.js
+++ controllers/homeroute.js
@@ -5,5 +5,14 @@ router.get('/', async (req, res) => {
   res.render('all');
 });
 
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+  }
+
+  res.render('signup');
+});
+
 
 module.exports = router;
