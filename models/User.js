86743d9d5a991abcf5577e4ad9b75c57f1be513b
Jescel James <jesceljames@gmail.com>
2024-01-22 20:42:49 -0500

    added bcrypt, password object

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+  return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -14,10 +19,20 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
   },  
   {
     sequelize,
-    modelName: 'user'
+    modelName: 'user',
+    freezeTableName: true,
+    underscored: true,
+
 
   }
 )
