cd6d3f773a41a6254995722e2232c55a02fa4580
Jescel James <jesceljames@gmail.com>
2024-01-22 20:46:44 -0500

    added hooks

--- models/User.js
+++ models/User.js
@@ -8,8 +8,7 @@ class User extends Model {
   }
 }
 
-User.init(
-  {
+User.init({
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -28,10 +27,21 @@ User.init(
     }
   },  
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+      },
+      async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+      }
+    },
     sequelize,
     modelName: 'user',
     freezeTableName: true,
     underscored: true,
+    timestamps: false,
 
 
   }
